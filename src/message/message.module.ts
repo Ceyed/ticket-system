5fb14bece52a2cb5999fb15e3085fa0c8a1c7317
Ceyed <saeed.sarmad.976@gmail.com>
2024-11-19 09:40:11 +0330

    feat: message service

--- src/message/message.module.ts
+++ src/message/message.module.ts
@@ -5,10 +5,11 @@ import { MessageRepository } from 'common/database/postgres/entities/message/mes
 import { IamModule } from 'src/iam/iam.module';
 import { TicketModule } from 'src/ticket/ticket.module';
 import { MessageGateway } from './message.gateway';
+import { MessageService } from './message.service';
 
 @Module({
     imports: [IamModule, TicketModule, TypeOrmModule.forFeature([MessageEntity])],
-    providers: [MessageGateway, MessageRepository],
+    providers: [MessageGateway, MessageService, MessageRepository],
     exports: [MessageGateway],
 })
 export class MessageModule {}
